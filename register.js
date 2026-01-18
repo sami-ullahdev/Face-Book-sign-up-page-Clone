5ecd8c74429d89a96a6a4317a6fc333376a11286
sami-ullahdev <samiullahdeveloperr@gmail.com>
2026-01-18 23:37:48 +0500

    let item done from html by id

---
+++ register.js
@@ -0,0 +1,10 @@
+console.log("Js Running");
+
+let firstname = document.getElementById("firstname");
+let lastname = document.getElementById("lastname");
+let day = document.getElementById("day");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+let genders = document.getElementById("genders");
+let email = document.getElementById("email");
+let submitbtn = document.getElementById("submitbtn");
