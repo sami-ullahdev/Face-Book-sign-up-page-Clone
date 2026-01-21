11e2f246148010b767a5e7a639b925685023440f
sami-ullahdev <samiullahdeveloperr@gmail.com>
2026-01-22 00:20:02 +0500

    get all id's in js file

---
+++ Login/login.js
@@ -0,0 +1,6 @@
+console.log("Js Running");
+
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let passwordeye = document.getElementById("password-eye")
+let loginsubmit = document.getElementById("login-btn")
