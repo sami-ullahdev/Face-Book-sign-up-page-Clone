d3cbdad77cd50c8f8b08ee5b0b5cc293eb65d28f
sami-ullahdev <samiullahdeveloperr@gmail.com>
2026-01-22 00:21:07 +0500

    Git sweet alert funtion from web of sweet alert

--- Login/login.js
+++ Login/login.js
@@ -4,3 +4,10 @@ let email = document.getElementById("email")
 let password = document.getElementById("password")
 let passwordeye = document.getElementById("password-eye")
 let loginsubmit = document.getElementById("login-btn")
+
+function ShowAlert(icon, text) {
+    Swal.fire({
+        icon: icon,
+        text: text
+    });
+}
